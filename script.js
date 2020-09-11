f1a0af100446e5bc1d0be74d2ae71ed7d38ddcc5
Roman Ilyushin <47747654+rojoil@users.noreply.github.com>
2020-09-11 15:09:42 +0300

    Using API

--- script.js
+++ script.js
@@ -1,25 +1,108 @@
-let coordX = '1231px';
+'use strict';
 
-$('.ball').on('click', function () {
-	function getRandomInRange(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-	let getCoordY = getRandomInRange(0,602);
-	let coordY = getCoordY + 'px';
-	$(this).css('transform',`translate(${coordX},${coordY})`);
-	switch (coordX) {
-		case '1231px':
-			coordX = '0px';
-			break
-		case '0px':
-			coordX = '1231px';
-			break
-	}
-	function goalAlert() {
-		if ((getCoordY < 345) && (getCoordY > 265)) {
-			alert('ГОЛ!');
+const select = document.querySelector('select');
+const input = document.querySelector('input');
+const btn = document.getElementById('search_request_btn');
+const closeBtnA = document.querySelector('.close-btn_a');
+const closeBtnB = document.querySelector('.close-btn_b');
+const closeBtnC = document.querySelector('.close-btn_c');
 
-		}
-	}
-	setTimeout(goalAlert, 1000);
-});
+btn.onclick = () => {
+  const api = "https://swapi.dev/api/";
+
+  let url = api + select.value + '/?search=';
+  url += input.value;
+
+  let request = new XMLHttpRequest();
+
+  request.addEventListener("load", () => {
+    const response = JSON.parse(request.response); 
+
+    if (request.status !== 200) {
+      alert(
+        'Произошла ошибка при получении ответа от сервера:\n\n' +
+          response.message
+      );
+      return;
+    }
+
+    if (response.count == 0) {
+      alert("К сожалению, данные не получены по запросу: " + url);
+      return;
+    } else {
+      for (let record in response.results) {
+        let html = document.createElement('li');
+        html.innerHTML = response.results[record]['name'];
+        document.querySelector('.search_result').insertAdjacentElement('beforeend', html);
+      }
+    }
+  });
+
+  request.open("get", url);
+  request.send();
+  document.querySelector('ul').innerHTML = '';
+}
+
+document.querySelector('ul').onclick = function(ev) {
+  const api = "https://swapi.dev/api/";
+
+  let searchTypeCode = select.value;
+  let url = api + searchTypeCode + '/?search=';
+  url += ev.target.textContent;
+
+  let request = new XMLHttpRequest();
+
+  request.onload = () => {
+    const response = JSON.parse(request.response);
+    if (request.status !== 200) {
+      alert(
+        'Произошла ошибка при получении ответа от сервера:\n\n' +
+          response.message
+      );
+      return;
+    }
+
+    if (response.count == 0) {
+      alert("К сожалению, данные не получены по запросу: " + url);
+      return;
+    } else if (searchTypeCode == 'people') {
+      document.querySelector('.person_data').classList.toggle('response-disabled');
+      const record = response.results[0];
+      document.getElementById('name').innerHTML = record.name;
+      document.getElementById('height').innerHTML = record.height;
+      document.getElementById('mass').innerHTML = record.mass;
+      document.getElementById('birth_year').innerHTML = record.birth_year;
+      document.getElementById('films_count').innerHTML = record.films.length;
+    } else if (searchTypeCode == 'planets') {
+      document.querySelector('.planet_data').classList.toggle('response-disabled');
+      const record = response.results[0];
+      document.getElementById('planet_name').innerHTML = record.name;
+      document.getElementById('planet_climate').innerHTML = record.climate;
+      document.getElementById('planet_terrain').innerHTML = record.terrain;
+      document.getElementById('planet_population').innerHTML = record.population;
+    } else if (searchTypeCode == 'starships') {
+      document.querySelector('.ship_data').classList.toggle('response-disabled');
+      const record = response.results[0];
+      document.getElementById('ship_name').innerHTML = record.name;
+      document.getElementById('ship_model').innerHTML = record.model;
+      document.getElementById('ship_manufacturer').innerHTML = record.manufacturer;
+      document.getElementById('ship_hyperdrive_rating').innerHTML = record.hyperdrive_rating;
+  }
+}
+
+  request.open("get", url);
+  request.send();
+
+  closeBtnA.onclick = () => {
+    closeBtnA.parentElement.classList.toggle('response-disabled');
+  }
+
+  closeBtnB.onclick = () => {
+    closeBtnB.parentElement.classList.toggle('response-disabled');
+  }
+
+  closeBtnC.onclick = () => {
+    closeBtnC.parentElement.classList.toggle('response-disabled');
+  }
+
+}
